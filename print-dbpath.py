09e960ccacc2f4124359e896403c76193417c51e
Sabry Razick <sabryr@gmail.com>
2021-06-10 16:55:25 +0200

    Detect database location string presence

--- print-dbpath.py
+++ print-dbpath.py
@@ -4,13 +4,13 @@
 import sys
 from os import path
 import re
+#modloadmsg = "Database location : /cluster/shared"
 
 def post_package_hook(self):
 	if len(sys.argv) >1:
 		eb_file = sys.argv[1]
 		print("Arguments of the script : ", eb_file)	
 		if path.exists(eb_file):
-			print("File found ", eb_file)
 			append_info(eb_file)
 		else:
 			print("Not a file ", eb_file)
@@ -18,7 +18,6 @@ def post_package_hook(self):
 		print("no arguments")
 
 def append_info(filepath):
-	print("file path=", filepath)
 	content = None
 	with open(filepath, "r") as f:
 		content = f.readlines()
@@ -31,8 +30,11 @@ def append_info(filepath):
 	if len(moduleclasses)>0:
 		module_class_loc=content.index(moduleclasses[-1])
 		database_locs=list(filter(r2.match, content))
-		if not len(database_locs)>0:
-			print("Database location not found")
+		print(len(database_locs))
+		if len(database_locs)==0:
+			print("It seems you are trying to install a module that needs reference data")
+			print("We have already setup this data in a central location")
+			print("Please include the following line in the easybuild file and then then do the instlation again")
 
 	
 def test_print_dbpath():
